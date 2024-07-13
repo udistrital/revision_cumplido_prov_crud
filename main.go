157c6b0c602641105ad532a85f244c793afdb473
faidercamilo26 <olayafaider@gmail.com>
2024-07-12 22:14:47 -0500

    feature: Se crea el api_crud del modulo de revision_cumplidos_proveedores. udistrital/Cumplidos_y_certificaciones_documentacion#80

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	_ "github.com/udistrital/revision_cumplidos_proveedores_crud/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+	auditoria "github.com/udistrital/auditoria"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerrorv2"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "postgres", "postgres://"+
+		beego.AppConfig.String("PGuser")+":"+
+		beego.AppConfig.String("PGpass")+"@"+
+		beego.AppConfig.String("PGhost")+":"+
+		beego.AppConfig.String("PGport")+"/"+
+		beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+
+		beego.AppConfig.String("PGschema")+"")
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	auditoria.InitMiddleware()
+	beego.ErrorController(&customerrorv2.CustomErrorController{})
+	apistatus.Init()
+	beego.Run()
+}
---
+++ models/cambio_estado_cumplido.go
@@ -0,0 +1,157 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type CambioEstadoCumplido struct {
+	Id                  int                `orm:"column(id);pk;auto"`
+	EstadoSoporteId     *EstadoSoporte     `orm:"column(estado_soporte_id);rel(fk)"`
+	CumplidoProveedorId *CumplidoProveedor `orm:"column(cumplido_proveedor_id);rel(fk)"`
+	ResponsableId       int                `orm:"column(responsable_id);null"`
+	CargoReponsable     string             `orm:"column(cargo_reponsable);null"`
+	FechaCreacion       time.Time          `orm:"column(fecha_creacion);type(timestamp without time zone);null"`
+	FechaModificacion   time.Time          `orm:"column(fecha_modificacion);type(timestamp without time zone);null"`
+	Activo              bool               `orm:"column(activo);null"`
+}
+
+func (t *CambioEstadoCumplido) TableName() string {
+	return "cambio_estado_cumplido"
+}
+
+func init() {
+	orm.RegisterModel(new(CambioEstadoCumplido))
+}
+
+// AddCambioEstadoCumplido insert a new CambioEstadoCumplido into database and returns
+// last inserted Id on success.
+func AddCambioEstadoCumplido(m *CambioEstadoCumplido) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetCambioEstadoCumplidoById retrieves CambioEstadoCumplido by Id. Returns error if
+// Id doesn't exist
+func GetCambioEstadoCumplidoById(id int) (v *CambioEstadoCumplido, err error) {
+	o := orm.NewOrm()
+	v = &CambioEstadoCumplido{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+// GetAllCambioEstadoCumplido retrieves all CambioEstadoCumplido matches certain condition. Returns empty list if
+// no records exist
+func GetAllCambioEstadoCumplido(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(CambioEstadoCumplido)).RelatedSel()
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []CambioEstadoCumplido
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdateCambioEstadoCumplido updates CambioEstadoCumplido by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateCambioEstadoCumplidoById(m *CambioEstadoCumplido) (err error) {
+	o := orm.NewOrm()
+	v := CambioEstadoCumplido{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeleteCambioEstadoCumplido deletes CambioEstadoCumplido by Id and returns error if
+// the record to be deleted doesn't exist
+func DeleteCambioEstadoCumplido(id int) (err error) {
+	o := orm.NewOrm()
+	v := CambioEstadoCumplido{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&CambioEstadoCumplido{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
