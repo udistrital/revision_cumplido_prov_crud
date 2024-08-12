b8efcaad8af8c3aec9575a6e1d2825cbac44817f
JuanCamiloMartinezLopez <juacmartinezl@correo.udistrital.edu.co>
2024-08-12 12:41:52 -0500

    feat: se ajusta el archivo de entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export INSCRIPCION_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/inscripcion_crud/db/username --output text --query Parameter.Value)"
-  export INSCRIPCION_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/inscripcion_crud/db/password --output text --query Parameter.Value)"
+  export REVISION_CUMPLIDOS_PROOVEDORES_CRUD_PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/revision_cumplido_prov_crud/db/username --output text --query Parameter.Value)"
+  export REVISION_CUMPLIDOS_PROOVEDORES_CRUD_PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/revision_cumplido_prov_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
